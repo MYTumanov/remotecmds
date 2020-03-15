d75cd38003498508b23db44d713a3bd4e44bbf0d
MYTumanov <MYTumanov@yandex.ru>
2020-03-15 23:16:31 +0300

    change download

--- download/download.go
+++ download/download.go
@@ -11,6 +11,10 @@ import (
 
 // https://golang.org/lib/godoc/images/footer-gopher.jpg
 
+func init() {
+	log.SetPrefix("Download:")
+}
+
 type dStatus int
 
 const (
@@ -20,33 +24,56 @@ const (
 	err
 )
 
+var dStatusToStr = map[dStatus]string{
+	queue:      "queue",
+	inprogress: "inprogress",
+	complete:   "complete",
+	err:        "err",
+}
+
 type downloads struct {
 	fileName     string
 	filePath     string
 	url          string
 	status       dStatus
-	byteDownload int
+	byteDownload int64
 }
+
+var downloadsList map[string]*downloads
 
 // Download ownloads files from url to path
 func Download(urlStr string, path string) {
-	log.SetPrefix("Download:")
-	resp, err := http.Get(urlStr)
+	if downloadsList == nil {
+		downloadsList = make(map[string]*downloads)
+	}
+	newDownload := &downloads{
+		fileName: getName(urlStr),
+		filePath: path,
+		url:      urlStr,
+		status:   queue,
+	}
+	downloadsList[urlStr] = newDownload
+	newDownload.download()
+
+}
+
+func (d *downloads) download() {
+	d.status = inprogress
+	resp, err := http.Get(d.url)
 	if err != nil {
 		log.Println(err)
 	}
 	defer resp.Body.Close()
 
-	fName := getName(urlStr)
-	fullTempPath := filepath.Join(path, fName+".temp")
-	fullPath := filepath.Join(path, fName)
+	fullTempPath := filepath.Join(d.filePath, d.fileName+".temp")
+	fullPath := filepath.Join(d.filePath, d.fileName)
 	f, err := os.Create(fullTempPath)
 	if err != nil {
 		log.Println(err)
 	}
 	defer f.Close()
 
-	_, err = io.Copy(f, resp.Body)
+	d.byteDownload, err = io.Copy(f, resp.Body)
 	if err != nil {
 		log.Println(err)
 	}
@@ -59,10 +86,23 @@ func Download(urlStr string, path string) {
 	if err := os.Rename(fullTempPath, fullPath); err != nil {
 		log.Println(err)
 	}
+	d.status = complete
 }
 
 func getName(url string) string {
 	name := url[strings.LastIndex(url, "/")+1:]
 	log.Println(name)
 	return name
+}
+
+// GetDownloadList return list of downloads
+func GetDownloadList() []string {
+	if downloadsList == nil {
+		return nil
+	}
+	var list []string
+	for _, d := range downloadsList {
+		list = append(list, d.fileName+" "+dStatusToStr[d.status])
+	}
+	return list
 }
