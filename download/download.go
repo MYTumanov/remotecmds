e74f021b9aa3bebba8db26929ab749a9ad44df56
Maksim Tumanov <MYTumanov@yandex.ru>
2020-03-24 16:49:59 +0300

    in progres

--- download/download.go
+++ download/download.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -63,11 +64,12 @@ func Download(urlStr string, path string) {
 		downloadsList = make(map[string]*downloads)
 	}
 	newDownload := &downloads{
-		fileName: getName(urlStr),
-		filePath: path,
-		url:      urlStr,
-		status:   queue,
-		stopLoad: make(chan bool),
+		fileName:     getName(urlStr),
+		filePath:     path,
+		url:          urlStr,
+		status:       queue,
+		stopLoad:     make(chan bool),
+		byteDownload: 0,
 	}
 	downloadsList[urlStr] = newDownload
 	err := newDownload.download()
@@ -80,16 +82,27 @@ func Download(urlStr string, path string) {
 
 func (d *downloads) download() error {
 	d.status = inprogress
-	resp, err := http.Get(d.url)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", d.url, nil)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	req.Header.Set("Range", "bytes="+strconv.FormatInt(d.byteDownload, 10)+"-")
+
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Println(err)
 		return err
 	}
 	defer resp.Body.Close()
 
+	// Work with file
+
 	fullTempPath := filepath.Join(d.filePath, d.fileName+".temp")
 	fullPath := filepath.Join(d.filePath, d.fileName)
-	f, err := os.Create(fullTempPath)
+	// f, err = os.Create(fullTempPath)
+	f, err := os.OpenFile(fullTempPath, os.O_APPEND|os.O_CREATE, 0666)
 	if err != nil {
 		log.Println(err)
 		return err
@@ -147,3 +160,15 @@ func GetDownloadList() []string {
 
 // StopDownload stop download by id
 func StopDownload() {}
+
+// ResumeDownload resumes given download
+func ResumeDownload(d string) {
+	if dwnld, ok := downloadsList[d]; ok && dwnld.status == stop {
+		err := dwnld.download()
+		if err != nil {
+			log.Println(err)
+		}
+		return
+	}
+	log.Println("Not found download: " + d)
+}
--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	server := &sr.Server{
 		Addr:        ":8080",
-		IdleTimeout: 20 * time.Second,
+		IdleTimeout: 200 * time.Second,
 		MaxBuffer:   64,
 		MaxRead:     1024,
 	}
